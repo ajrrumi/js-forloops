52c473cd109a8a65c3e6b0912e6f5917f2fe5e5b
Alavi Rumi <alavirumi@Alavis-MacBook-Pro.local>
2018-03-14 19:05:32 -1000

    Problem #2

--- exercises.js
+++ exercises.js
@@ -32,6 +32,7 @@ function popCharts(){
   }
 }
 popCharts();
+
 /* 3) Dead Presidents
 Declare a variable named `presidents` and assign it to an array containing the following Presidents: Washington, Adams, Jefferson, Madison and Monroe.
  
